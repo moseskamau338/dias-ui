7b056ddf137ac13452d1468816fae41d179142f6
Moses Kamau <moseskamau338@gmail.com>
2023-05-21 16:52:27 +0300

    Add loader to transitions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <BaseLayout>
+  <BaseLayout v-show="!loading">
       <router-view v-slot="{ Component , route, params}">
         <Transition :key="route.path"
           enter-active-class="transform ease-out duration-300 transition delay-100 opacity-0"
@@ -12,18 +12,30 @@
         </Transition>
       </router-view>
   </BaseLayout>
+    <div v-show="loading" class="h-screen flex flex-col items-center">
+      <div class="h-screen text-xl flex justify-center items-center transition-all duration-700 flex-col">
+        <span class="flex">
+            <svg class="animate-spin -ml-1 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            Loading...
+        </span>
+      </div>
+    </div>
+
 </template>
 
 <script setup>
 import {useAuthStore} from "@/db/authentication";
 import { useRoute, useRouter } from 'vue-router';
-import isEmpty from "lodash/isEmpty";
-import {watch} from "vue";
+import {ref, watch} from "vue";
 
 // hooks/composables
 const route = useRoute(); // use this to access the current route and its properties
 const router = useRouter(); // use this to perform navigation actions such as .push and .go
 const authStore = useAuthStore();
+const loading = ref(true)
 
 // vars global to this component
 const isCurrentUserAuthed = authStore.authenticated();
@@ -38,14 +50,14 @@ if(isCurrentUserAuthed){
 
 // functions global to this component
 const handleUserAuth = (routeRequiresAuth, routeRequiresGuest) => {
-
+  loading.value = true
   /**
    * handles cases where the user is NOT authenticated but they're
    * trying to access a guarded route
    */
   if(routeRequiresAuth && !isCurrentUserAuthed){
     // redirect to the login page
-    //window.location.replace('/auth/login')
+    window.location.replace('/auth/login')
   }
 
   /**
@@ -57,11 +69,15 @@ const handleUserAuth = (routeRequiresAuth, routeRequiresGuest) => {
     //router.push({ name: 'dashboard' });
     window.location.replace('/')
   }
+
+    setTimeout(() => {
+      loading.value = false
+    }, 200)
 }
 
-//if(route.meta.requiresAuth || route.meta.requiresGuest){
-//  handleUserAuth(route.meta.requiresAuth, route.meta.requiresGuest)
-//}
+if(route.meta.requiresAuth || route.meta.requiresGuest){
+  handleUserAuth(route.meta.requiresAuth, route.meta.requiresGuest)
+}
 
 /**
  * watch for changes in route.meta
--- src/library/apiRegister.js
+++ src/library/apiRegister.js
@@ -1,35 +1,4 @@
-const client_id = 'fb778b78-2a27-4ec2-8ea0-4afdc000d0a8'
-
 export const apiRoutes = {
-    metrics: '/payment-links/payment-link/metrics/summary/150',
-    //users
-    authenticate: '/users/login/',
-    //register: '/users/',
-    refresh: '/users/renew-token',
-    reset: '/users/reset-password',
-    logout: '/users/logout',
-    profile: '/users/update-profile',
-    //clients
-    clients: '/accounts/client',
-    client: '/accounts/client',
-
-    //get API key
-    get_api_key:'/users/generate-api-key',
-
-    //transactions
-    transactions: '/transactions/',
-
-    //virtual Accounts
-    virtualAccounts: '/virtual_accounts/',
-
-    //prefixes
-    prefixes: '/accounts/prefix',
-
-    //tariffs
-    tariffs: '/accounts/bank-tariff',
-
-    //teams management
-    groups:client_id+'/groups',
-    members:'',
-    invite_user:'/invitation'
+    //auth
+    'register': 'signup',
 }
